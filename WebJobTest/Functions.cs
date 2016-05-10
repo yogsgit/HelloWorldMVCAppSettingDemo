14c47fcab4e62235c7beb0c128f273307d9b791e
yogsgit <yogs26a@hotmail.com>
2016-05-11 00:02:20 +0100

    changed schedule

--- WebJobTest/Functions.cs
+++ WebJobTest/Functions.cs
@@ -12,11 +12,11 @@ namespace WebJobTest
         // This function will be triggered based on the schedule you have set for this WebJob
         // This function will enqueue a message on an Azure Queue called queue
         [NoAutomaticTrigger]
-        public static void ManualTrigger(TextWriter log, int value, [Queue("queue")] out string message)
+        public static void ManualTrigger(TextWriter log, int value)
         {
-            log.WriteLine("Function is invoked with value={0}", value);
-            message = value.ToString();
-            log.WriteLine("Following message will be written on the Queue={0}", message);
+            log.WriteLine("Function is invoked with value");
+            //message = value.ToString();
+            //log.WriteLine("Following message will be written on the Queue={0}", message);
         }
     }
 }
